351c719a8b4d9d3dd689779a7bb0d2bbf8953122
SUPERDEV <rizurazz797@gmail.com>
2025-06-26 23:42:36 +0530

    Add files via upload

---
+++ emojis.js
@@ -0,0 +1,18 @@
+module.exports = {
+    success: "✅",
+    error: "⚠️",
+    music: "🎶",
+    queue: "🟰",
+    info: "⛔",
+    time: "⌛",
+    song: "🎧",
+    volume: "🎛️",
+    pause: "⏯️",
+    play: "▶️",
+    skip: "⏭️",
+    previous: "⏮️",
+    stop: "🛑",
+    shuffle: "🔀",
+    repeat: "🔂",
+    loading: "🔃"
+}; 
